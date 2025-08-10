28955f623c38aff066dfed02bf11d498e26bc428
Ricardo Chitagu <ricardoprintworks@gmail.com>
2025-08-10 20:26:12 +0200

    pdf upload and download

--- download.php
+++
@@ -1,7 +0,0 @@
-<?php
-$token = preg_replace('~[^a-f0-9]~','',$_GET['token'] ?? '');
-$file  = __DIR__."/runtime/$token.pdf";
-if(!$token || !is_file($file)){ http_response_code(404); exit('Missing'); }
-header('Content-Type: application/pdf');
-header('Content-Disposition: attachment; filename=\"merged_'.date('Ymd_His').'.pdf\"');
-readfile($file);
